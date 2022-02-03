2a4de3da7f789154781047d3fa06a0220b6f9bc4
Hnat Lesiv <hnat.lesiv@epfl.ch>
2022-02-03 08:15:03 +0100

    Refactored the code and split in uls and sls cases

---
+++ src/loads_uls.py
@@ -0,0 +1,23 @@
+import inputs as inp
+
+y_G = 1.35
+y_Q = 1.5
+
+p_sdl = 0.5 # kPa
+p_Q = 4 # kPa
+
+w_self = inp.A*10**-6 * inp.rho * inp.g / 1000 # kN/m
+w_sdl = p_sdl*inp.Lx / 2 / 1000
+w_G = w_self + w_sdl
+
+w_Q = p_Q * inp.Lx / 2 / 1000
+w = (w_G*y_G + w_Q*y_Q) # kN/m - N/mm - UDL
+
+P_G = 0.2 # kN
+P_Q = 4 # kN
+P = P_G*y_G + P_Q*y_G
+
+if __name__ == '__main__':
+    print(w_G, w_Q, w)
+
+
--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ import argparse
 import inputs as inp
 import fire
 import fem
+import loads_uls as uls
+import loads_sls as sls
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -14,15 +16,26 @@ if __name__ == '__main__':
 
     parser.add_argument(
         '-f', '--fire', type=bool,
-        help=('Whether to include fire in the analysis'
+        help=('Include fire in the analysis?'
               ' True/False'), default=False)
+    parser.add_argument(
+        '-l', '--load', type=str,
+        help=('Type of design'
+              ' Can be uls/sls'), default='uls')
     args = parser.parse_args()
 
     fy, E = inp.fy, inp.E
     if args.fire:
         fy, E = fire.temperature(plot=args.plot)
 
-    fem.model(fy=fy, E=E, plot=args.plot)  
+    if args.load == 'sls':
+        w = sls.w
+        P = sls.P
+    else:
+        w = uls.w
+        P = uls.P
+
+    fem.model(w=w, P=P, fy=fy, E=E, plot=args.plot)  
 
 
 
